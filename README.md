d5be37c72bd1237596aba16e8cbd552c47585052
SrTobi <code.databyte@gmail.com>
2016-02-24 11:52:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Techwiese RSS Extension
---
+++ src/extension.ts
@@ -0,0 +1,12 @@
+import * as vscode from 'vscode'; 
+
+export function activate(context: vscode.ExtensionContext) {
+
+	console.log('Congratulations, your extension "code-techwiese-rss" is now active!'); 
+
+	var disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+		vscode.window.showInformationMessage('Hello World!');
+	});
+	
+	context.subscriptions.push(disposable);
+}
