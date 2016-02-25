065227e5848b8010fea6fa3679af182ccd656a00
SrTobi <code.databyte@gmail.com>
2016-02-25 13:51:02 +0100

    changed document title

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,7 @@ var feedProvider = new FeedDocumentContentProvider(feedList);
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Extension "code-techwiese-rss" is now active!'); 
+	console.log('Extension "TechWiese.de-News-RSS" is now active!'); 
 
 	let disposable = vscode.commands.registerCommand('techwiese.show', cmdShow);
 	context.subscriptions.push(disposable);
@@ -20,5 +20,5 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 async function cmdShow() {
-    await vscode.commands.executeCommand("vscode.previewHtml", vscode.Uri.parse("feed://techwiese"));
+    await vscode.commands.executeCommand("vscode.previewHtml", vscode.Uri.parse("feed://techwiese/News & Infos für Entwickler – TechWiese.de – ein deutschsprachiges Online-Angebot für Entwickler von Microsoft"));
 }
